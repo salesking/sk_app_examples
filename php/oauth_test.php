e1a04e0b8d73f12c8cc9e5967883990c1bdf2c79
Georg Leciejewski <gl@salesking.eu>
2011-02-24 23:21:31 +0100

    addded php example

---
+++ php/oauth_test.php
@@ -0,0 +1,33 @@
+<?php
+
+  $app_id = "APP_CLIENT_ID";
+  $app_secret = "APP_SECRET";
+  $app_url = "http://localhost/oauth_test";
+  $app_scope = "api/clients:read";
+  $sk_url = "https://SUBDOMAIN.salesking.eu";
+
+  $code = $_REQUEST["code"];
+
+  if(empty($code)) { # redirect to authorize url
+    $dialog_url = $sk_url . "/oauth/authorize?" .
+                "client_id=". $app_id .
+                "&scope=" . urlencode($app_scope);
+                "&redirect_uri=" . urlencode($app_url);
+
+    echo("<script> top.location.href='" . $dialog_url . "'</script>");
+  }
+  # build url to get the access token
+  $token_url = $sk_url . "/oauth/access_token?" .
+              "client_id=". $app_id .
+              "&redirect_uri=" . urlencode($app_url) .
+              "&client_secret=" . $app_secret .
+              "&code=" . $code;
+  # GET and parse resonse json
+  $resp = json_decode(file_get_contents($token_url));
+  # build url
+  $usr_url = $sk_url . "/api/users/current?access_token=" . $resp->access_token;
+  # GET info about current user
+  $user = json_decode(file_get_contents($usr_url));
+  echo("King: " . $user->user->email);
+
+?>
